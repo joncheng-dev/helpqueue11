c604ba84f63db060760f38a40ba24105f0aa375b
Jonathan Cheng <jcheng461@gmail.com>
2022-12-04 16:01:05 +0900

    add ReusableForm component to be used with NewTicketForm. User can add new ticket to TicketList

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
 
-function NewTicketForm() {
+function NewTicketForm(props) {
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4(),
+    });
+  }
+
   return (
     <React.Fragment>
       <h2>New Ticket Form</h2>
+      <hr />
+      <ReusableForm onSubmittingForm={handleNewTicketFormSubmission} buttonText="Create New Ticket" />
     </React.Fragment>
   );
 }
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,30 +1,13 @@
 import React from "react";
 import Ticket from "./Ticket";
 
-function TicketList() {
-  const ticketList = [
-    {
-      names: "Homer and Bart",
-      location: "Moe's Tavern",
-      issue: "Car tire flat",
-    },
-    {
-      names: "Marge and Lisa",
-      location: "Try 'n Save",
-      issue: "Sold out of project parts",
-    },
-    {
-      names: "Patty and Selma",
-      location: "Springfield Elementary",
-      issue: "Krabapple out of smokes",
-    },
-  ];
+function TicketList(props) {
   return (
     <React.Fragment>
       <h2>Ticket List</h2>
       <hr />
-      {ticketList.map((ticket) => (
-        <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} />
+      {props.ticketList.map((ticket) => (
+        <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} id={ticket.id} key={ticket.id} />
       ))}
     </React.Fragment>
   );
